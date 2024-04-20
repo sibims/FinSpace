a17ff5e991ed1971ce6dbca3baf2fadcfa2e5d88
sibims <mssibi047@gmail.com>
2024-04-20 21:19:38 +0530

    added button functionalities

--- src/Components/AuthN/auth.jsx
+++ src/Components/AuthN/auth.jsx
@@ -1,21 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './auth.css'
 
 import { UilUser, UilEnvelopeAlt, UilPadlock } from '@iconscout/react-unicons'
 
 
 const AuthPage = () => {
+
+    const [action,setAction] = useState("Sign Up");
   return (
     <div className='container'>
         <div className="header">
-            <div className="text">Sign Up</div>
+            <div className="text">{action}</div>
             <div className="underline"></div>
         </div>
         <div className="inputs">
-            <div className="input">
+        {action==="Login"?<div></div>:<div className="input">
                 <UilUser style={{margin:"0px 30px"}}/>
                 <input type="text" placeholder="Name"></input>
-            </div>
+            </div>}
+            
             <div className="input">
                 <UilEnvelopeAlt style={{margin:"0px 30px"}}/>
                 <input type="email" placeholder="Email"></input>
@@ -24,11 +27,11 @@ const AuthPage = () => {
                 <UilPadlock style={{margin:"0px 30px"}}/>
                 <input type="password" placeholder='Password'></input>
             </div>
-            <div className="forgot-password">Lost Password? <span>Click Here!</span></div>
+            {action==="Sign Up"?<div></div>:<div className="forgot-password">Lost Password? <span>Click Here!</span></div>}
             <button className='continue'>Continue</button>
             <div className="submit-container">
-                <div className="submit">Sign Up</div>
-                <div className="submit">Login</div>
+                <div className={action==="Login"?"submit gray":"submit"} onClick={()=>{setAction("Sign Up")}}>Sign Up</div>
+                <div className={action==="Sign Up"?"submit gray":"submit"}onClick={()=>{setAction("Login")}}>Login</div>
             </div>
         </div>
     </div>
